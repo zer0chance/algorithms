42b99ee7615dfaa54a3596e4f1e702973585a462
zer0chance <ier7928@gmail.com>
2020-02-12 22:08:21 +0700

    Create backward part of Gauss method

--- matrix_gauss.cpp
+++ matrix_gauss.cpp
@@ -4,7 +4,9 @@
 
 #define N 3
 
-double M[N][N+1] = {{1, -2, 3, 2}, {3, 1, -1, 3}, {2, 5, 2, 9}}; 
+//double M[N][N+1] = {{1, -2, 3, 2}, {3, 1, -1, 3}, {2, 5, 2, 9}}; 
+double M[N][N+1] = {{-3, 1, 2, 1}, {2, 1, -3, -4}, {-4, 2, 1, -2}}; 
+
 
 using namespace std;
 
@@ -27,7 +29,8 @@ void print_matrix()
 int main(int argc, char** argv)
 {
     print_matrix();
-    //прямой ход
+
+    //Forward part
     for (int k = 0; k < N - 1; k++)
     {
         for (int i = k + 1; i < N; i++)
@@ -38,9 +41,38 @@ int main(int argc, char** argv)
                 M[i][j] += M[k][j] * ratio * -1;
             }
         }
-    }     
+    }    
+
+
+    //Making 1 in main diagonal
+    for (int k = 0; k < N; k++)
+    {
+        double elem = M[k][k];
+        for (int i = k; i <= N; i++)
+        {
+            M[k][i] /= elem;
+            // double ratio = M[i][k] / M[k][k];
+            // for (int j = k; j <= N; j++)
+            // {
+            //     M[i][j] += M[k][j] * ratio * -1;
+            // }
+        }
+    }    
+
     
-    //обратный ход
+    //Backward part
+    for (int k = N - 1; k >= 0; k--)
+    {
+        for (int i = k - 1; i >= 0; i--)
+        {
+            double ratio = M[i][k] / M[k][k];
+            for (int j = N; j >= k; j--)
+            {
+                M[i][j] += M[k][j] * ratio * -1;
+            }
+        }
+    }    
+
    
     
     print_matrix();
