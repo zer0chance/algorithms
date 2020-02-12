4fc5a5bb2f55beb1a64a3d67e943a48f4d6dae28
zer0chance <ier7928@gmail.com>
2020-02-12 21:43:20 +0700

    Create forward part of Gauss method

---
+++ matrix_gauss.cpp
@@ -0,0 +1,49 @@
+#include <cstdlib>
+#include <iostream> 
+#include <iomanip>
+
+#define N 3
+
+double M[N][N+1] = {{1, -2, 3, 2}, {3, 1, -1, 3}, {2, 5, 2, 9}}; 
+
+using namespace std;
+
+
+void print_matrix()
+{
+    for (int i = 0; i < N; i++)
+    {
+        cout << endl;
+        for (int j = 0; j <= N; j++)
+        {
+            cout << M[i][j] << "  ";
+        }
+    }
+    
+    cout << endl << endl;
+}
+
+
+int main(int argc, char** argv)
+{
+    print_matrix();
+    //прямой ход
+    for (int k = 0; k < N - 1; k++)
+    {
+        for (int i = k + 1; i < N; i++)
+        {
+            double ratio = M[i][k] / M[k][k];
+            for (int j = k; j <= N; j++)
+            {
+                M[i][j] += M[k][j] * ratio * -1;
+            }
+        }
+    }     
+    
+    //обратный ход
+   
+    
+    print_matrix();
+    
+    return 0;
+}
