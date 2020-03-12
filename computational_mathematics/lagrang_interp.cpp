adeef3623465becf2b8c49bf05d90fd2b34b2168
zer0chance <ier7928@gmail.com>
2020-03-12 12:38:50 +0700

    Lagrange interpolation polynomial

---
+++ computational_mathematics/lagrang_interp.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+#define N 4
+
+using namespace std;
+
+
+double X[N] = {1, 2, 3, 4};
+double Y[N] = {1.0000, 1.4142, 1.7321, 2.0000};
+
+
+double P3(double x)
+{
+    double result = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        double fraction = 1;  
+        for (int j = 0; j < N; j++)
+        {
+            if (i != j)
+                fraction *= (x - X[j]) / (X[i] - X[j]);    
+        } 
+        result += Y[i] * fraction;
+    }
+
+    return result;    
+}
+
+
+int main()
+{
+    printf(" \n\n Result: %lf\n\n", P3(static_cast<double>(2.56)));
+
+    return 0;
+}
